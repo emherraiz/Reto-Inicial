196fbb45b6d6715dbdb5a850a812b92e1a06ab39
Enrique Marina <eherraiz@myuax.com>
2022-09-12 03:38:01 +0200

    seguimos

--- dama.py
+++ dama.py
@@ -1,5 +1,13 @@
 import numpy as np
 n = 3
 tablero = np.zeros((n,n))
+tablero_prueba = [[1, 0, 0, 0],[0, 0, 1, 0],[1, 0, 0, 0],[0, 0, 0, 1]]
 
+def dama_atacada(tablero):
+    for fila in tablero:
+        if sum(fila) > 1:
+            return True
+        if
+
+print(dama_atacada(tablero_prueba))
 
